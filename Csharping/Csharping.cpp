ca24bdc26dc210f5f6f4b9301a9bbe37d9868ff8
Svyat <svyatoslavlot@gmail.com>
2023-12-21 00:45:09 +0200

    new function

--- Csharping/Csharping.cpp
+++ Csharping/Csharping.cpp
@@ -4,11 +4,14 @@ using namespace std;
 void print() {
     cout << "HHH" <<endl;
 }
+void miSHAAAA() {
+    cout << "MIsha ti shhoo pyknyl???" << endl;
 
+}
 
 int main()
 {
     print();
-    //
+    miSHAAAA();
 }
 
